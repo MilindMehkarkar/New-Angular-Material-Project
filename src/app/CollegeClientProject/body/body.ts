d3630c6d9b0a0819ca762014b831a0affdc88321
Milind Mehkarkar <milinduijs@gmail.com>
2018-10-07 05:36:38 -0700

    Initial commit

---
+++ src/app/CollegeClientProject/body/body.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:'body-app',
+    templateUrl:'./body.html',
+    styleUrls:['./body.css']
+})
+
+export class BodyApp{
+    
+}
